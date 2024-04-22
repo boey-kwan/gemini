12a42124732dd12d49ff9196e46657b62323d5dc
boeyk <boeyk@sfu.ca>
2024-04-22 04:20:48 -0700

    Add today and memories pages.

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { Outlet, BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Today from "./components/Today";
+import Memories from "./components/Memories";
+import "./App.css";
+
+const App = () => {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <div className="body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/today" element={<Today />} />
+            <Route path="/memories" element={<Memories />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App
