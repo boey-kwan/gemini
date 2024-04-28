4a9836901ca4c0ec247e4deab65a74ef815a294f
boeyk <boeyk@sfu.ca>
2024-04-27 23:52:15 -0700

    Update task fields

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,15 +1,26 @@
 import '../App.css'
 import { useState } from 'react';
 import { Checkbox, TextField } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 export default function Task() {
 
     const [checked, setChecked] = useState(false);
-    const [date, setDate] = useState([]);
-    const [time, setTime] = useState([]);
-    const [location, setLocation] = useState([]);
-    const [description, setDescription] = useState([]);
-    const [image, setImage] = useState([]);
+    
+    // Whether or not the user has enabled each field
+    const [showDescriptionField, setShowDescriptionField] = useState(false);
+    const [showTimeField, setShowTimeField] = useState(false);
+    const [showLocationField, setShowLocationField] = useState(false);
+    const [showImageField, setShowImageField] = useState(false);
+
+    // The current content of each field
+    const [description, setDescription] = useState("desc");
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+    const [location, setLocation] = useState("loc");
+    const [image, setImage] = useState("im");
 
     return (
       <div>
@@ -17,29 +28,35 @@ export default function Task() {
         <div className="task">
 
             <div className='row' style={{height: 'fit-content'}}>
-                <TextField id='task-title-field' label="Task title" variant='outlined' fullWidth/>
+                <TextField 
+                    id='task-title-field' 
+                    label="Task title" 
+                    variant='outlined'
+                    fullWidth/>
+
+                    {/* todo: change field height, and make it bold */}
 
                 <Checkbox 
                     checked={checked} 
                     onClick={() => {
                         setChecked(!checked);
                     }}
-                />
+                    />
 
             </div>
             <form className="task-details">
                 
-                {/* Date */}
-                <div>
-                    <label for="date">Date:</label>
-                    <input type="date" id="date" name="date"/>
-                </div>
+                {/* Time Range */}
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <div className='row' style={{justifyContent: 'flex-start', columnGap: '1em'}}>
+                        From
+                        <TimePicker value={startTime} onChange={(x) => { if (x) setStartTime(x) }}/>
+                        to
+                        {/* Todo: Make end time optional */}
+                        <TimePicker value={endTime} onChange={(x) => { if (x) setEndTime(x) }}/>
+                    </div>
+                </LocalizationProvider>
                 
-                {/* Time */}
-                <div>
-                    <label for="time">Time:</label>
-                    <input type="time" id="time" name="time"/>
-                </div>
                 {/* Location */}
                 <div>
                     <label for="location">Location:</label>
