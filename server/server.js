f24bbba19a9cb59d89010a69e1f0d45b71ee8f99
TJ <tjgrewal.dev@gmail.com>
2024-04-29 21:37:34 -0700

    added gemini api call with example

--- server/server.js
+++ server/server.js
@@ -1,15 +1,66 @@
 import express from "express";
 import cors from "cors";
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+import dotenv from 'dotenv';
 
-import usersRouter from "./routes/users.js";
+dotenv.config();
+// import usersRouter from "./routes/users.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+const MODEL_NAME = "gemini-1.5-pro-latest";
+const API_KEY = process.env.G_KEY;
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/users", usersRouter);
+// app.use("/users", usersRouter);
+
+
+async function runChat() {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 1,
+    topK: 0,
+    topP: 0.95,
+    maxOutputTokens: 8192,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [
+    ],
+  });
+
+  const result = await chat.sendMessage("Tell me about Pokemon in under 100 words");
+  const response = result.response;
+  console.log(response.text());
+}
+
+runChat();
 
 
 // start the Express server
