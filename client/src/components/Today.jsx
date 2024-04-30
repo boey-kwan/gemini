2d07b541f121eec8961440469005a914ec62a922
ack11 <ack11@sfu.ca>
2024-04-30 08:04:40 -0700

    Add ability to delete task

--- client/src/components/Today.jsx
+++ client/src/components/Today.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import '../App.css';
 import Task from "./Task";
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
 export default function Today() {
 
@@ -13,23 +14,50 @@ export default function Today() {
     setCurrentTaskId(id);
     console.log("current task: " + id)
   }
+
+  function deleteTaskWithId(id) {
+    console.log(taskList.filter(value => value !== id))
+    setTaskList(taskList.filter(value => value !== id))
+  }
  
     return (
       <div>
         <div className='row'>
-          <div className="body-left">
-          {"< Yesterday"}
-          </div>
+          <div className="body-left"> {"< Yesterday"} </div>
 
           <div className="body-center scroll">
             <h1 className='h1' style={{textAlign: 'center'}}>Today's To-Do List</h1>
             <div className='task-list'>
-              {taskList.map((value) => {
-                return <Task key={value} 
-                id={value} 
-                showSidebar={currentTaskId == value}
-                onClick={updateCurrentTaskId}/>
-              })}
+              { taskList.length ? taskList.map((value) => {
+                  return <Task 
+                            key={value} 
+                            id={value} 
+                            showSidebar={currentTaskId == value}
+                            onClick={updateCurrentTaskId}
+                            deleteTask={deleteTaskWithId}/>
+                }) : <h2 style={{fontStyle: 'italic', opacity: 0.5, textAlign: 'center'}}>Create a task to get started!</h2>
+              }
+            </div>
+
+            {/* Add a task button */}
+            <div 
+              style={{ 
+                display: 'flex', 
+                justifyContent: 'center',
+              }}>
+                <button 
+                  className='row' 
+                  style={{
+                    width: 'fit-content',
+                  }}
+                  onClick={() => {
+                    setTaskList(
+                      [...taskList, taskList.length+1]
+                    )
+                  }}>
+                  <AddOutlinedIcon/>
+                  <div>Add task</div>
+                </button>
             </div>
           </div>
 
@@ -37,18 +65,6 @@ export default function Today() {
           </div>
         </div>
 
-        {/* Add a task button */}
-        <button style={{
-          display: 'block',
-          margin: 'auto',
-          alignText: 'center'}}
-          onClick={() => {
-            setTaskList(
-              [...taskList, taskList.length+1]
-            )
-          }}>
-          Add a task.
-        </button>
       </div>
     );
   }
