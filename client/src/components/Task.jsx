8cff788dd3029b3772422780a4a8271d07c3e210
ack11 <ack11@sfu.ca>
2024-05-01 03:57:15 -0700

    Move trashcan to sidebar

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -4,7 +4,11 @@ import { Checkbox, TextField } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import PhotoOutlinedIcon from '@mui/icons-material/PhotoOutlined';
+import PlaceOutlined from '@mui/icons-material/PlaceOutlined';
 import Sidebar from './Sidebar';
 
 export default function Task(props) {
@@ -38,16 +42,19 @@ export default function Task(props) {
                 ref={taskRef}
                 onClick={()=>props.onClick(props.id)}
             >
-                <div className='row' style={{height: 'fit-content'}}>
+                <div className='row' style={{ height: 'fit-content' }}>
                     <TextField 
                         id='task-title-field' 
                         label="Task title" 
                         variant='outlined'
+                        size='small'
+                        style={{width:'calc(100% - 2.5em)'}}
                         fullWidth/>
 
                         {/* todo: change field height, and make it bold */}
 
-                    <Checkbox 
+                    <Checkbox
+                        className='checkbox' 
                         checked={checked} 
                         onClick={() => {
                             setChecked(!checked);
@@ -55,29 +62,38 @@ export default function Task(props) {
                         />
                 </div>
                 
-                <form className="task-details">
+                <form className="task-details" style={{width:'calc(100% - 2.5em)'}}>
 
                     {/* Time Range */}
                     {
                         showFields.time ? 
+                        <div className='row' style={{ height: 'fit-content' }}>
+                            <AccessTimeIcon style={{ margin: '0.1em' }}/>
                             <LocalizationProvider dateAdapter={AdapterDayjs} style={{visibility: true}}>
                             <div className='row' style={{justifyContent: 'flex-start', columnGap: '1em'}}>
                                 From
-                                <TimePicker value={startTime} onChange={(x) => { if (x) setStartTime(x) }}/>
+                                <TimePicker slotProps={{ textField: { size: 'small' } }} value={startTime} onChange={(x) => { if (x) setStartTime(x) }}/>
                                 to
                                 {/* Todo: Make end time optional */}
-                                <TimePicker value={endTime} onChange={(x) => { if (x) setEndTime(x) }}/>
+                                <TimePicker slotProps={{ textField: { size: 'small' } }} value={endTime} onChange={(x) => { if (x) setEndTime(x) }}/>
                             </div>
                         </LocalizationProvider>
+                        </div>
                         : null
                     }
                     
                     {/* Location */}
                     {
                         showFields.location ?
-                        <div>
-                            <label htmlFor="location">Location:</label>
-                            <input type="text" id="location" name="location"/>
+                        <div className='row' style={{ height: 'fit-content', justifyContent: 'start' }}>
+                            <PlaceOutlined style={{ margin: '0.1em' }}/>
+                            <TextField 
+                                id='task-title-field' 
+                                label="Location" 
+                                variant='outlined'
+                                size='small'
+                                fullWidth/>
+
                         </div>
                         : null
                     }
@@ -86,9 +102,14 @@ export default function Task(props) {
                     {/* Description */}
                     {
                         showFields.description ?
-                        <div>
-                            <label htmlFor="description">Description:</label>
-                            <input type="text" id="description" name="description"/>
+                        <div className='row' style={{ height: 'fit-content' }}>
+                            <AutoStoriesOutlinedIcon style={{ margin: '0.1em' }}/>
+                            <TextField 
+                                id='task-title-field' 
+                                label="Description" 
+                                variant='outlined'
+                                size='small'
+                                fullWidth/>
                         </div>
                         : null
                     }
@@ -96,21 +117,21 @@ export default function Task(props) {
                     {/* Image */}
                     {
                         showFields.image ?
-                        <div>
+                        <div className='row' style={{ height: 'fit-content', justifyContent: 'start' }}>
+                            <PhotoOutlinedIcon style={{ margin: '0.1em' }}/>
                             <label htmlFor="image">Image:</label>
                             <input type="image" id="image" name="image"/>
                         </div>
                         : null
                     }
                 </form>
-                
-                <button className='trashcan' onClick={() => props.deleteTask(props.id)} style={{position: 'relative', float: 'right'}}>
+                {/* <button className='trashcan' onClick={() => props.deleteTask(props.id)} style={{ marginTop: '0.5em', position: 'relative', float: 'right'}}>
                     <DeleteOutlinedIcon/>
-                </button>
+                </button> */}
 
             </div>
 
-            <Sidebar onClickSidebarIcon={onClickSidebarIcon} showSidebar={props.showSidebar} showFields={showFields}/>
+            <Sidebar onClickSidebarIcon={onClickSidebarIcon} showSidebar={props.showSidebar} showFields={showFields} deleteTask={() => props.deleteTask(props.id)}/>
             </div>
             
         </>
